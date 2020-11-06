4434c815c5dc218dc349da6d8e3ceb23a5fecaaf
jazielinho <jahazielponce@gmail.com>
2020-11-06 10:25:45 +0100

    tarea 38: Biologo

---
+++ tarea_38/biologo/readme.md
@@ -0,0 +1,11 @@
+El código está escrito en Python
+El diagrama está compuesto de 3 partes:
+* valida_secuencia: valida si una secuencia es válida
+* devuelve_secuencias: a partir de dos secuencias devuelve la secuencia menor, mayor y sus longitudes
+* Biologo: función principal, a partir de dos secuencias, devuelve la secuencia común.
+
+* valida_secuencia:
+
+* devuelve_secuencias:
+
+* Biologo:
--- tarea_38/biologo/tarea_38_biologo_python.py
+++ tarea_38/biologo/tarea_38_biologo_python.py
@@ -1,102 +1,118 @@
-'''
-Created on 25 jun. 2020
-@author: jazielinho
-'''
 
 import string
 
 
-def verifica_secuencia(secuencia):
+def es_secuencia_valida(secuencia):
     '''
-    Verifica si una secuencia es correcta
+    Verifica si una secuencia es valida
     :param secuencia: secuencia a verificar
     :return: True o False si la secuencia pasa la validación
     '''
     try:
-        if len(secuencia) == 0:
-            # Al menos texto
-            print(f"{secuencia} necesita texto")
+        len_secuencia = len(secuencia)
+        puntuaciones = string.punctuation
+        if len_secuencia > 0:
+            contador = 0
+            valido = True
+            while valido:
+                if contador >= len_secuencia:
+                    break
+                else:
+                    palabra = secuencia[contador]
+                    if palabra.isspace():
+                        print('{} tiene espacio en blanco'.format(secuencia))
+                        valido = False
+                    else:
+                        if palabra.isdigit():
+                            print('{} tiene numeros'.format(secuencia))
+                            valido = False
+                        else:
+                            if palabra in puntuaciones:
+                                print('{} tiene puntuaciones'.format(secuencia))
+                                valido = False
+                            else:
+                                contador += 1
+            return valido
+        else:
+            print('{} es vacio'.format(secuencia))
             return False
-        for palabra in secuencia:
-            if palabra.isspace():
-                # no espacios en blanco
-                print(f"{secuencia} tiene espacios en blanco")
-                return False
-            if palabra.isdigit():
-                # no digitos
-                print(f"{secuencia} hay digitos")
-                return False
-            if palabra in string.punctuation:
-                # no signos de puntuación
-                print(f"{secuencia} tiene signos de puntuación")
-                return False
-        return True
     except Exception as e:
-        print(f"Error en verifica_secuencia {e}")
+        print('Error en es_secuencia_valida, secuencia {}: {}'.format(secuencia, e))
         return False
 
 
 def devuelve_secuencias(secuencia_1, secuencia_2):
     '''
-    Devuelve la longitud máxima, la secuencia mayor y secuencia menor
-    :param secuencia_1: secuencia mínima
-    :param secuencia_2: secuencia máxima
-    :return: longitud, secuencia_minima, secuencia_maxima
+    Devuelve la longitud menor, mayor, la secuencia menor y secuencia mayor
+    :return: True si el proceso sale bien, caso contrario devuelve False
     '''
-    len_1 = len(secuencia_1)
-    len_2 = len(secuencia_2)
-    if len_1 > len_2:
-        return len_2, secuencia_2, secuencia_1
-    return len_1, secuencia_1, secuencia_2
-
-
-def verifica_secuencia_texto(secuencia_comun, secuencia_comparar):
-    '''
-    verifica si la secuencia común se encuentr adentro de la secuencia_comparar
-    :param secuencia_comun: secuencia base a buscar similitud
-    :param secuencia_comparar: secuencia donde se va a buscar
-    :return:
-    '''
-
-    # se puede escribir (secuencia_comun in secuencia_comparar) y va más rápido, pero quiero hacerlo desde cero
-
-    for inicio in range(len(secuencia_comparar) - len(secuencia_comun) + 1):
-        secuencia_compara_ = secuencia_comparar[inicio: (inicio + len(secuencia_comun))]
-        if secuencia_comun == secuencia_compara_:
-            return True
-    return False
+    len_sec_1 = len(secuencia_1)
+    len_sec_2 = len(secuencia_2)
+    if len_sec_1 > len_sec_2:
+        longitud_menor = len_sec_2
+        longitud_mayor = len_sec_1
+        secuencia_menor = secuencia_2
+        secuencia_mayor = secuencia_1
+    else:
+        longitud_menor = len_sec_1
+        longitud_mayor = len_sec_2
+        secuencia_menor = secuencia_1
+        secuencia_mayor = secuencia_2
+    return longitud_menor, longitud_mayor, secuencia_menor, secuencia_mayor
 
 
 def devolver_secuencia_comun(secuencia_1, secuencia_2):
     '''
     Tarea 38: devuelve secuencia máxima comun entre dos secuencias
-    :param secuencia_1: primera secuencia
-    :param secuencia_2: segunda secuencia
     :return: secuencia común de mayor longitud
     '''
     try:
-        if (verifica_secuencia(secuencia=secuencia_1) is False) or (verifica_secuencia(secuencia=secuencia_2) is False):
-            return None
+        secuencia_comun = None
 
-        longitud_analisis, secuencia_minima, secuencia_maxima = devuelve_secuencias(secuencia_1=secuencia_1,
-                                                                                    secuencia_2=secuencia_2)
+        valido_secuencia_1 = es_secuencia_valida(secuencia=secuencia_1)
+        valido_secuencia_2 = es_secuencia_valida(secuencia=secuencia_2)
 
-        for longitud in reversed(range(longitud_analisis)):
-            for inicio in range(longitud_analisis - longitud):
-                secuencia_comun = secuencia_minima[inicio: (inicio + longitud + 1)]
-                if verifica_secuencia_texto(secuencia_comun=secuencia_comun, secuencia_comparar=secuencia_maxima):
-                    return secuencia_comun
+        if valido_secuencia_1 is False or valido_secuencia_2 is False:
+            return secuencia_comun
 
-        return ''
+        longitud_menor, longitud_mayor, secuencia_menor, secuencia_mayor = devuelve_secuencias(secuencia_1=secuencia_1,
+                                                                                               secuencia_2=secuencia_2)
+
+        contador_final = longitud_menor
+        contador_inicio = 0
+
+        existe_comun = False
+
+        while existe_comun is False:
+
+            if contador_inicio >= contador_final:
+                contador_final -= 1
+                contador_inicio = 0
+                if contador_final <= 0:
+                    break
+            else:
+                secuencia_comun = secuencia_menor[contador_inicio: contador_final]
+
+                if secuencia_comun in secuencia_mayor:
+                    existe_comun = True
+                else:
+                    contador_inicio += 1
+
+        if existe_comun is False:
+            return ''
+        else:
+            return secuencia_comun
+
     except Exception as e:
-        print(f"Error en devolver_secuencia_común {e}")
-        return False
-    
+        print('Error en devolver_secuencia_comun: {}'.format(e))
+        return None
+
 
 def main():
     secuencia_1 = input("Ingrese primera secuencia:\t")
     secuencia_2 = input("Ingrese segunda secuencia:\t")
     secuencia_comun = devolver_secuencia_comun(secuencia_1=secuencia_1, secuencia_2=secuencia_2)
+
     if secuencia_comun is not None:
         if len(secuencia_comun) > 0:
             print(f'Secuencia común: "{secuencia_comun}"')
