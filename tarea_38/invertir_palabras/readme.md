24a4328489346167e1ef2a2c9190577a8870be26
jazielinho <jahazielponce@gmail.com>
2020-11-07 11:03:07 +0100

    tarea 38: invierte texto

---
+++ tarea_38/invertir_palabras/readme.md
@@ -0,0 +1,6 @@
+El código está escrito en Python
+El diagrama está compuesto de 2 partes:
+* invierte_texto: Invierte un texto
+* invierte_lista_textos: Invierte una lista de textos
+
+![](https://raw.githubusercontent.com/Jazielinho/theegg_ai/master/tarea_38/biologo/devuelve_secuencias.png)
--- tarea_38/invertir_palabras/tarea_38_invertir_python.py
+++ tarea_38/invertir_palabras/tarea_38_invertir_python.py
@@ -4,36 +4,42 @@ Created on 25 jun. 2020
 '''
 
 
-def invierte_texto(texto, caso_num):
+def invierte_texto(texto):
     '''
     Función que dado un texto, divide en espacios en blanco e invierte el orden
     :param texto: Texto
-    :param caso_num: Indice de numeración
     :return: Texto en orden inverso. La división es por espacios en blanco
     '''
     try:
         texto_lista = texto.split()
         texto_reverso_lista = reversed(texto_lista)
         texto_reverso = ' '.join(texto_reverso_lista)
-        return f"Case #{caso_num}: {texto_reverso}"
+        return texto_reverso
     except Exception as e:
-        print(f"Error in invierte_texto {e}")
+        print(f"Error in invierte_texto, texto: {texto}: {e}")
         return None
 
 
-def invierte_texto_lista(lista_textos):
+def invierte_lista_textos(lista_textos):
     '''
     Función que invierte el orden de cada texto dentro de una lista de textos
     :param lista_textos: lista de textos originales
-    :return: lista donde cada elemento es el texto en orden invertido
+    :return: mensaje con lista de palabras en orden inverso
     '''
     try:
-        lista_texto_invertido = [invierte_texto(x, enum + 1) for enum, x in enumerate(lista_textos)]
-        if None in lista_texto_invertido:
-            return None
-        return '\n'.join(lista_texto_invertido)
+        len_lista_textos = len(lista_textos)
+
+        index_ = 0
+
+        mostrar = ''
+
+        while index_ < len_lista_textos:
+            texto_reverso = invierte_texto(texto=lista_textos[index_])
+            mostrar = mostrar + f'\nCase # {(index_ + 1)}: {texto_reverso}'
+            index_ += 1
+        return mostrar
     except Exception as e:
-        print(f"Error en invierte_texto_lista: {e}")
+        print(f"Error in invierte_lista_textos, lista_textos: {lista_textos}: {e}")
         return None
 
 
@@ -45,7 +51,7 @@ def main():
             lista_textos = []
             for i in range(num_textos):
                 lista_textos.append(input(f"Ingrese texto {(i + 1)}:\t"))
-            lista_texto_invertido = invierte_texto_lista(lista_textos=lista_textos)
+            lista_texto_invertido = invierte_lista_textos(lista_textos=lista_textos)
             if lista_texto_invertido is not None:
                 print(lista_texto_invertido)
         else:
