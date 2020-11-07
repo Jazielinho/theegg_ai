ba7da92985b8e6e63916457e3a7be574aee0ed44
jazielinho <jahazielponce@gmail.com>
2020-11-07 12:21:27 +0100

    tarea 38: palindromo

---
+++ tarea_38/palindromo_primo/readme.md
@@ -0,0 +1,7 @@
+El código está escrito en Python
+El diagrama está compuesto de 3 partes:
+* es_primo: verifica si un número es primo
+* es_palindromo: verifica si un número es palindromo
+* devuelve_palindromo_primo_cercano: Devuelve el mayor núnmro más cercano que sea primo y palíndromo
+
+![](https://raw.githubusercontent.com/Jazielinho/theegg_ai/master/tarea_38/invertir_palabras/Invierte%20textos.png)
--- tarea_38/palindromo_primo/tarea_38_palindromo_primo_python.py
+++ tarea_38/palindromo_primo/tarea_38_palindromo_primo_python.py
@@ -4,7 +4,7 @@ Created on 25 jun. 2020
 '''
 
 
-def verifica_primo(numero):
+def es_primo(numero):
     '''
     Verificamos si un numero es primo
     :param numero: numero
@@ -13,7 +13,9 @@ def verifica_primo(numero):
     if numero < 2:
         # numero 1 o menor no son primos
         return False
-    for divisor in range(2, numero):
+    divisor = 2
+
+    while divisor < numero:
         if numero % divisor == 0:
             # Si el resto de la división es cero, el número no es primo
             return False
@@ -22,10 +24,12 @@ def verifica_primo(numero):
             # No es necesario verificar con todos los números posibles
             # https://bit.ly/3hCPPxG
             return True
+        divisor += 1
+
     return True
 
 
-def verifica_palindromo(numero):
+def es_palindromo(numero):
     '''
     Verifica si un número palíndromo
     :param numero: número
@@ -42,7 +46,7 @@ def verifica_palindromo(numero):
 
 def devuelve_palindromo_primo_cercano(numero):
     '''
-    Devuelve el mayor nýnero más cercano que sea primo y palíndromo
+    Devuelve el mayor número más cercano que sea primo y palíndromo
     :param numero: número
     :return: número mayor más cercano palíndromo y primo
     '''
@@ -51,9 +55,9 @@ def devuelve_palindromo_primo_cercano(numero):
     while encontrado is False:
         # Empezamos con el primer número
         numero_mayor += 1
-        if verifica_palindromo(numero=numero_mayor):
+        if es_palindromo(numero=numero_mayor):
             # Verificamos primero si es palíndromo (es más rápido que verificar si es primo)
-            if verifica_primo(numero=numero_mayor):
+            if es_primo(numero=numero_mayor):
                 # Verificamos si el número es primo
                 encontrado = True
     return numero_mayor
